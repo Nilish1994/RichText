2a6b853a99853f8aaaf0c9f995a5688f69d9da19
nilish994 <nilish.aluthwatta@1billiontech.com>
2023-05-02 21:37:11 +0530

    disabled drag text

--- RichTextController/Components/Description/RichTextEditor.tsx
+++ RichTextController/Components/Description/RichTextEditor.tsx
@@ -2,74 +2,28 @@ import React, {
   useState,
   useEffect,
   ClipboardEvent,
-  // useCallback,
   useRef,
 } from "react";
 import ReactQuill from "react-quill";
-// import * as Quill from "quill";
 import "react-quill/dist/quill.snow.css";
-// import "react-quill/dist/quill.bubble.css";
 
 import "../../css/RichTextController.css";
 
-// type FormatPainterProps = {
-//   quill: typeof Quill;
-// };
-
-const RichTextEditor = ({ quill }: any, props: any) => {
+const RichTextEditor = ({ quill }: any) => {
   const [value, setValue] = useState("");
   const quillRef = useRef<ReactQuill>(null);
   const divRef = useRef(null);
 
-  const preventSelection = (event: any) => {
-    if (!event.shiftKey) {
-      event.preventDefault();
-    }
-  }
-
-
-  // useEffect(() => {
-  //   const handleContextMenu = (e: any) => {
-  //     e.preventDefault();
-  //   };
-  //   document.addEventListener("contextmenu", handleContextMenu);
-  //   document.addEventListener("selectstart", preventSelection);
-
-  //   return () => {
-  //     document.removeEventListener("contextmenu", handleContextMenu);
-  //     document.removeEventListener("selectstart", preventSelection);
-  //   };
-  // }, []);
-
   useEffect(() => {
-    if (divRef.current) {
-      document.addEventListener("selectstart", preventSelection);
-    }
+    const handleContextMenu = (e: any) => {
+      e.preventDefault();
+    };
+    document.addEventListener("contextmenu", handleContextMenu);
+
     return () => {
-      document.removeEventListener("selectstart", preventSelection);
+      document.removeEventListener("contextmenu", handleContextMenu);
     };
   }, []);
-
-  // const handleFormatPainter = () => {
-  //   const quill = quillRef.current?.getEditor();
-  //   console.log("quillRef", quillRef);
-  //   console.log("quill", quill);
-  //   if (quill) {
-  //     const selection = quill.getSelection();
-  //     if (selection) {
-  //       const format: any = quill.getFormat(selection);
-  //       console.log("format", format);
-  //       quill.clipboard.dangerouslyPasteHTML(selection.index, format, "silent");
-  //     }
-  //   }
-  // };
-
-  // const handleBeforeInput = (event: any, info: any) => {
-  //   const selection = quillRef.current?.getEditor().getSelection();
-  //   if (selection) {
-  //     event.preventDefault();
-  //   }
-  // };
 
   function dragOver(ev: any) {
     ev.preventDefault();
@@ -77,10 +31,6 @@ const RichTextEditor = ({ quill }: any, props: any) => {
 
   function drop(ev: any) {
     ev.preventDefault();
-    // const droppedItem = ev.dataTransfer.getData("drag-item");
-    // if (droppedItem) {
-    //   props.onItemDropped(droppedItem);
-    // }
   }
 
   const preventCopyPaste = (e: ClipboardEvent<HTMLInputElement>) => {
@@ -94,14 +44,8 @@ const RichTextEditor = ({ quill }: any, props: any) => {
       [{ header: [1, 2, 3, 4, 5, 6, false] }],
       ["bold", "italic", "underline"],
       [{ color: [] }, { background: [] }],
-      // [{ script: "sub" }, { script: "super" }],
-      // ["blockquote", "code-block"],
       [{ list: "ordered" }, { list: "bullet" }],
-      // [{ indent: "-1" }, { indent: "+1" }, { align: [] }],
-      // ["link", "image", "video"],
-      // ["clean"],
     ],
-    // dragAndDrop: false,
     clipboard: { matchVisual: false },
   };
 
@@ -117,7 +61,6 @@ const RichTextEditor = ({ quill }: any, props: any) => {
       >
         <ReactQuill modules={modules} onChange={setValue} ref={quillRef} />
       </div>
-      {/* <button onClick={handleFormatPainter}>Format Painter</button> */}
     </>
   );
 };
